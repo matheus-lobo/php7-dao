8c66da229ca6a4dea681b193b29c2a5bf0b86bfa
Matheus <matheusbrbg@gmail.com>
2018-08-08 11:52:17 -0300

    Criamos a Classe Usuario

--- class/sql.php
+++ class/sql.php
@@ -7,10 +7,10 @@ class Sql extends PDO {
 	public function __construct(){
 		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 	}
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 		foreach($parameters as $key => $value){
 
-			$this->setParam($key, $value);
+			$this->setParam($statement, $key, $value);
 		}
 	}
 
--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+$root = new Usuario();
+$root->loadbyId(1);
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios ");
-
-echo json_encode($usuarios);
+echo $root;
 
 
 ?>
